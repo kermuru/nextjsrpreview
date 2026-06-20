a5463d44b483f2a45e2a949e2a5a8e3b278513d2
Kervin Fugata <151914582+kermuru@users.noreply.github.com>
2026-06-20 11:07:40 +0800

    env update
    env update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,8 @@
 // const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+
+#live cred
 const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'https://api.rp-vespera.cloud/api';
+
 // const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'https://staging.rp-vespera.cloud/api';
 //const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'https://springgreen-jellyfish-261481.hostingersite.com/api';
 
